46767a48ae6f0428c8b9eeb0c5cdc4cef1d34248
Celso Santos <zedblade.santos@gmail.com>
2020-07-10 20:58:58 +0100

    Remove template_file by filetemplate

--- modules/api-gateway/gloo-ee/main.tf
+++ modules/api-gateway/gloo-ee/main.tf
@@ -11,40 +11,6 @@ resource "kubernetes_namespace" "gloo_system" {
   }
 }
 
-data "template_file" "gloo_ee_values_template" {
-  template = file("${path.module}/tmpl/values.tmpl")
-
-  vars = {
-    access_logger_enabled = var.access_logger_enabled
-    license_key           = var.license_key
-    discovery_enabled     = var.discovery_enabled
-    discovery_fds_mode    = var.discovery_fds_mode
-    discovery_stats       = var.discovery_stats
-    gateway_enabled       = var.gateway_enabled
-    gateway_stats         = var.gateway_stats
-    grafana_password      = var.grafana_password
-    grafana_url           = var.grafana_url
-    grafana_username      = var.grafana_username
-    knative_enabled       = var.knative_enabled
-    knative_version       = var.knative_version
-    prometheus_url        = var.prometheus_url
-    stats                 = var.stats
-  }
-
-  depends_on = [
-    kubernetes_namespace.gloo_system
-  ]
-}
-
-resource "local_file" "gloo_ee_values_render" {
-  content  = data.template_file.gloo_ee_values_template.rendered
-  filename = "${path.cwd}/generated/gloo-ee/values.yaml"
-
-  depends_on = [
-    template_file.gloo_ee_values_template
-  ]
-}
-
 resource "helm_release" "gloo_ee" {
   name       = "gloo-ee"
   repository = "http://storage.googleapis.com/gloo-ee-helm"
@@ -54,7 +20,22 @@ resource "helm_release" "gloo_ee" {
   atomic     = true
 
   values = [
-    "${file("${path.cwd}/generated/gloo-ee/values.yaml")}"
+    "${templatefile("${path.module}/tmpl/values.tmpl", {
+      access_logger_enabled = var.access_logger_enabled
+      license_key           = var.license_key
+      discovery_enabled     = var.discovery_enabled
+      discovery_fds_mode    = var.discovery_fds_mode
+      discovery_stats       = var.discovery_stats
+      gateway_enabled       = var.gateway_enabled
+      gateway_stats         = var.gateway_stats
+      grafana_password      = var.grafana_password
+      grafana_url           = var.grafana_url
+      grafana_username      = var.grafana_username
+      knative_enabled       = var.knative_enabled
+      knative_version       = var.knative_version
+      prometheus_url        = var.prometheus_url
+      stats                 = var.stats
+    })}"
   ]
 
   set_sensitive {
@@ -66,16 +47,3 @@ resource "helm_release" "gloo_ee" {
     local_file.gloo_ee_values_render
   ]
 }
-
-# resource "null_resource" "gloo_gateway" {
-#   provisioner "local-exec" {
-#     # environment = {
-#     #   KUBECONFIG = "${path.root}/creds/config"
-#     # }
-#     command = "/usr/local/bin/helm install gloo-ee ${path.module}/gloo-ee/ --atomic -f ${path.root}/generated/gloo-ee/values.yaml --namespace ${var.namespace} --set-string license_key=${var.license_key}"
-#   }
-#   depends_on = [
-#     local_file.gloo_ee_values_render
-#   ]
-# }
-
